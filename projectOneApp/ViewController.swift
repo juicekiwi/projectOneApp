c00f052a9fe1f47eb187322f0a9436dbab59a13f
juicekiwi <vicaro@rambler.ru>
2021-12-07 23:49:53 +0300

    Initial Commit

---
+++ projectOneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  projectOneApp
+//
+//  Created by Виктория Салмина on 07.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
